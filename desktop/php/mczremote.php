b42f0ccce2839cd65b9851526b23f90cf79d2299
henribi <59314443+henribi@users.noreply.github.com>
2023-10-15 17:40:28 +0200

    images and config menu

--- desktop/php/mczremote.php
+++ desktop/php/mczremote.php
@@ -17,10 +17,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
                 <span>{{Configuration}}</span>
             </div>
             <div class="cursor eqLogicAction logoSecondary" id="bt_DownloadTemplate">
-                <i class="fas fa-file-export"></i>
+                <i class="fas fa-cloud-download-alt"></i>
                 <br>
                 <span>{{Télécharger le template}}</span>
             </div>
+<!----
             <div class="cursor eqLogicAction logoSecondary" id="bt_InstallTemplate">
                 <i class="fas fa-file-export"></i>
                 <br>
@@ -31,6 +32,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
                 <br>
                 <span>{{Installer template & créer jMQTT equipment}}</span>
             </div>
+--->
             <div class="cursor eqLogicAction logoSecondary" id="bt_healthmczremote">
                 <i class="fas fa-hands-helping"></i>
                 <br>
